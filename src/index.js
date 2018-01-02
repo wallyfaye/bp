12c152eaa6ecc2883deac76092a8525df45a7126
wallyfaye <wally.faye@gmail.com>
2018-01-02 10:23:26 -0500

    redux

--- src/index.js
+++ src/index.js
@@ -1,26 +1,40 @@
 import React from 'react'
-import { render } from 'react-dom'
+import ReactDOM from 'react-dom'
+
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+
+import createHistory from 'history/createBrowserHistory'
 import {
     BrowserRouter as Router,
     Route,
     Link
 } from 'react-router-dom'
 
+import { ConnectedRouter, routerReducer, routerMiddleware, push } from 'react-router-redux'
+
 import Bundle from './Bundle.js'
 import About from 'bundle-loader?lazy!./About'
 
-const BasicExample = () => (
-    <Router>
-        <div>
-            <ul>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/about">About</Link></li>
-            </ul>
-            <hr/>
-            <Route exact path="/" component={Home}/>
-            <Route path="/about" component={loadAbout}/>
-        </div>
-    </Router>
+const history = createHistory()
+const middleware = routerMiddleware(history)
+const store = createStore(
+  combineReducers({
+    router: routerReducer
+  }),
+  applyMiddleware(middleware)
+)
+
+const SampleApp = () => (
+    <div>
+        <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/about">About</Link></li>
+        </ul>
+        <hr/>
+        <Route exact path="/" component={Home}/>
+        <Route path="/about" component={loadAbout}/>
+    </div>
 )
 
 const Home = () => (
@@ -41,5 +55,11 @@ var element = document.createElement('div');
 element.setAttribute("id", "root");
 document.body.appendChild(element);
 
-
-render(<BasicExample />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <SampleApp />
+    </ConnectedRouter>
+  </Provider>,
+  document.getElementById('root')
+)
